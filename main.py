2e051d056b1f03ba18ad4137936b21e542d6da77
aligned64 <113694089+aligned64@users.noreply.github.com>
2024-12-14 13:28:54 +0200

    dataset import

---
+++ main.py
@@ -0,0 +1,10 @@
+import kagglehub
+
+# Download latest version
+DATASET_PATH = kagglehub.dataset_download("andrewmvd/leukemia-classification")
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
